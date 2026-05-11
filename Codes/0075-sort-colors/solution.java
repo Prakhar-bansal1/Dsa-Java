f6541db9d7b9563093aa610c2dcd9a1289902fb8
Prakhar Bansal <161673987+Prakhar-bansal1@users.noreply.github.com>
2026-05-11 01:41:24 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 42.9 MB (99.11%)

---
+++ Codes/0075-sort-colors/solution.java
@@ -0,0 +1,25 @@
+class Solution {
+    public void sortColors(int[] nums) {
+        int i=0;
+        int j=0;
+        int k=nums.length-1;
+        while(j<=k){
+            if(nums[j]==1){
+                j++;
+            }
+            else if(nums[j]==2){
+                int temp=nums[j];
+                nums[j]=nums[k];
+                nums[k]=temp;
+                k--;
+            }
+            else{
+                int temp = nums[j];
+                nums[j]=nums[i];
+                nums[i]=temp;
+                i++;
+                j++;
+            }
+        }
+    }
+}
